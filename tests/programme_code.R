647f049d1c38608a14214a4f58ea74718690b533
FERTE Thomas <fertet@chu-bordeaux.fr>
2024-10-08 18:23:42 +0200

    package structure

---
+++ tests/programme_code.R
@@ -0,0 +1,33 @@
+# ###
+# library(tidyverse)
+# library(dplyr)
+# library(gtsummary)
+# library(labelled)
+# library(rlang)
+# library(tidyr)
+# ### Chargement des tables
+#
+# load("I:/Etudes/DENOST Quentin GRECCAR 17 2020-021/04 - Aspects data management/08_Base/8.2_Bases figées/20240326/GRECCAR17_tab_2024-03-26.rds")
+#
+# ### Theme gtsummary -----
+# theme_gtsummary_compact()
+# theme_gtsummary_language("fr", decimal.mark = ".", big.mark = " ")
+#
+#
+#
+# #### sourcer la fonction desc_function
+#
+# source("desc_fonction6.R")
+#
+# # source("R:/EQUIPES/USMR/Activité/Activités biostatistiques/Fonctions standards R/Fonction_SMH/desc_fonction6.R")
+# ###
+#
+#
+# hm_1 <- hm %>% dplyr::left_join( rd %>% select(1, rdgrp), by =  "usubjid")
+#
+# ic_1 <- ic %>% dplyr::left_join( rd %>% select(1, rdgrp), by =  "usubjid")
+#
+#
+# ###
+#
+# source("code_test.R", encoding = 'UTF-8')
