e21d89340a55598bf2d6a9cd415bc752c2c8a582
MOHAMED HOUSSEIN Sahardid <mohasah@chu-bordeaux.fr>
2024-11-26 18:12:25 +0100

    Réorganisation de la fonction

--- tests/code_test.R
+++ tests/code_test.R
@@ -1,27 +1,15 @@
-# look_for(hm_1)
-#
-# hm_hm <- hm_1 %>% select(hmirmn)
-# str(hm_hm$hmirmn)
-#
-# group = "ALL"
-#
-# his_mal <- tbl_stack(
-#   list(
-#
-#     ## Filtre sur l'IRM réalisée à la pré-inclusion
-#
-#     hm_1 %>% filter(hmirmbr == "Oui") %>% select( hmtrpamo,19:22) %>%
-#       desc_var_6(table_title = "**Histoire de la maladie**",group = group,var_group =   hmtrpamo, group_title = "**Traitement**"),
-#
-#
-#     hm_1 %>% filter(hmirm == "Oui") %>% select( hmtrpamo,24:28) %>%
-#       desc_var_6(group = group,var_group = hmtrpamo),
-#
-#
-#
-#     hm_1 %>% filter(hmirm == "Oui") %>% select( hmtrpamo,hmirmga) %>%
-#       desc_var_6(group = group,var_group = hmtrpamo)), quiet = TRUE)
-#
-#
-# his_mal
-#
+
+
+
+
+data %>%
+  desc_var(table_title = "test",
+             quanti = "echelle",
+             var_group = "traitement",
+             group_title = "traitement",
+             group = "ALL")
+
+
+
+
+
---
+++ tests/data_test.R
@@ -0,0 +1,44 @@
+
+
+### Packages
+
+
+library(tidyverse)
+library(dplyr)
+library(gtsummary)
+library(labelled)
+library(rlang)
+library(tidyr)
+
+### Chargement des tables
+
+
+
+# Charger le package nécessaire
+set.seed(123)  # Pour garantir la reproductibilité
+
+# Création du data frame
+data <- data.frame(
+  Age = c(rnorm(45, mean = 50, sd = 10), rep(NA, 5)),  # Renommée Age
+  sexe = sample(c(0, 1), 50, replace = TRUE, prob = c(0.6, 0.4)),  # Renommée sexe
+  quatre_modalites = sample(c("A", "B", "C"), 50, replace = TRUE, prob = c(0.2, 0.5, 0.3)),  # Modalités sans "D"
+  traitement = sample(c("BRAS-A", "BRAS-B"), 50, replace = TRUE, prob = c(0.55, 0.45)),  # Nouvelle variable traitement
+  echelle = sample(0:5, 50, replace = TRUE)  # Nouvelle variable entière de 0 à 5
+)
+
+# Ajouter la modalité "D" comme niveau sans effectif
+data$quatre_modalites <- factor(data$quatre_modalites, levels = c("A", "B", "C", "D"))
+
+# Ajouter des labels à la variable sexe
+data$sexe <- factor(data$sexe, levels = c(0, 1), labels = c("Femme", "Homme"))
+
+# Aperçu des données
+
+
+
+
+data <- data %>% labelled::set_variable_labels( Age = "Age",
+                                                sexe = "sexe",
+                                                traitement  = "traitement",
+                                                quatre_modalites = "quatres niveaux",
+                                                echelle = "Echelle")
