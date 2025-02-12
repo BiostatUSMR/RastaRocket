208ffd3dbfc2bb6f1630d580d9328e8a3f2b0e77
FERTE Thomas <fertet@chu-bordeaux.fr>
2025-02-12 17:03:11 +0100

    new ei function

--- R/desc_ei_per_soc.R
+++ R/desc_ei_per_soc.R
@@ -1,3 +1,5 @@
+# main function -----------------------------------------------------------
+
 #' desc_ei_per_soc
 #' 
 #' A function to describe AE by soc and pt
@@ -10,129 +12,181 @@
 #' @export
 #'
 #' @examples
-
-
-library(gt)
-library(tidyverse)
-
-df_soc_pt <- data.frame(soc = c("Arrhythmia", "Myocardial Infarction", "Pneumonia", "Sepsis"),
-                        pt = c("Cardiac Disorders", "Cardiac Disorders", "Infections", "Infections"))
-
-df_pat_grp <- data.frame(id_pat = paste0("ID_", 1:10),
-                         grp = c(rep("A", 3), rep("B", 3), rep("C", 4)))
-
-df_pat_soc <- data.frame(id_pat = c("ID_1", "ID_1",
-                                    "ID_2",
-                                    "ID_4",
-                                    "ID_9"),
-                         soc = c("Arrhythmia", "Myocardial Infarction",
-                                 "Arrhythmia", "Pneumonia",
-                                 "Pneumonia"))
-
-desc_ei_per_soc(df_soc_pt = df_soc_pt,
-                df_pat_grp = df_pat_grp,
-                df_pat_soc = df_pat_soc)
-
+#' df_soc_pt <- data.frame(
+#'   soc = c("Arrhythmia",
+#'           "Myocardial Infarction",
+#'           "Pneumonia",
+#'           "Sepsis"),
+#'   pt = c("Cardiac Disorders",
+#'          "Cardiac Disorders",
+#'          "Infections",
+#'          "Infections")
+#' )
+#' 
+#' df_pat_grp <- data.frame(id_pat = paste0("ID_", 1:10),
+#'                          grp = c(rep("A", 3), rep("B", 3), rep("C", 4)))
+#' 
+#' df_pat_soc <- data.frame(id_pat = c("ID_1", "ID_1",
+#'                                     "ID_2",
+#'                                     "ID_4",
+#'                                     "ID_9"),
+#'                          soc = c("Arrhythmia", "Myocardial Infarction",
+#'                                  "Arrhythmia", "Pneumonia",
+#'                                  "Pneumonia"))
+#' 
+#' desc_ei_per_soc(df_soc_pt = df_soc_pt,
+#'                 df_pat_grp = df_pat_grp,
+#'                 df_pat_soc = df_pat_soc)
+#' 
 desc_ei_per_soc <- function(df_soc_pt,
                             df_pat_grp,
                             df_pat_soc){
   
-  ##### Total
+  ##### Build augmented df, Total is a whole new group
   
-  ### Build augmented df, Total is a whole new group
+  augmented_df_pat_grp <- dplyr::bind_rows(df_pat_grp,
+                                           df_pat_grp |>
+                                             dplyr::mutate(grp = "Total"))
   
-  df_pat_pt_soc_grp <- df_pat_soc |> 
-    left_join(df_pat_grp, by = "id_pat") |> 
-    left_join(df_soc_pt, by = "soc")
+  vec_grp <- unique(augmented_df_pat_grp$grp)
   
-  augmented_df_pat_soc_grp <- bind_rows(df_pat_pt_soc_grp,
-                                        df_pat_pt_soc_grp |>
-                                          mutate(grp = "Total"))
+  augmented_df_pat_soc_grp <- df_pat_soc |> 
+    dplyr::left_join(augmented_df_pat_grp,
+                     by = "id_pat",
+                     relationship = "many-to-many") |> 
+    dplyr::left_join(df_soc_pt,
+                     by = "soc")
   
-  augmented_df_pat_grp <- bind_rows(df_pat_grp,
-                                    df_pat_grp |>
-                                      mutate(grp = "Total"))
+  ##### Build wide dataframe
   
-  ### compute summary statistics per group
-  
-  df_nb_pat_per_grp <- augmented_df_pat_grp |> 
-    group_by(grp) |> 
-    summarise(nb_pat_per_grp = n())
-  
-  df_ei_total <- augmented_df_pat_soc_grp |> 
-    group_by(grp) |> 
-    summarise(nb_ei = n(),
-              pct_ei = 100,
-              nb_pat = length(unique(id_pat)),
-              .groups = "drop") |> 
-    left_join(df_nb_pat_per_grp, by = "grp") |> 
-    mutate(pct_pat = nb_pat/nb_pat_per_grp*100,
-           pt = "Total",
-           soc = "Total") |> 
-    select(pt, soc, grp, nb_ei, pct_ei, nb_pat, pct_pat)
-  
-  ##### SOC
-  
-  df_ei_pt <- augmented_df_pat_soc_grp |> 
-    group_by(grp, pt) |> 
-    summarise(nb_ei = n(),
-              nb_pat = length(unique(id_pat)),
-              .groups = "drop") |> 
-    left_join(df_ei_total |> 
-                select(grp, nb_ei_denom = nb_ei),
-              by = "grp") |> 
-    left_join(df_nb_pat_per_grp,
-              by = "grp") |> 
-    mutate(pct_pat = nb_pat/nb_pat_per_grp*100,
-           pct_ei = nb_ei/nb_ei_denom*100,
-           soc = "Total") |> 
-    select(pt, soc, grp, nb_ei, pct_ei, nb_pat, pct_pat)
-  
-  df_ei_soc <- augmented_df_pat_soc_grp |> 
-    group_by(grp, soc, pt) |> 
-    summarise(nb_ei = n(),
-              nb_pat = length(unique(id_pat)),
-              .groups = "drop") |> 
-    left_join(df_ei_total |> 
-                select(grp, nb_ei_denom = nb_ei),
-              by = "grp") |> 
-    left_join(df_nb_pat_per_grp,
-              by = "grp") |> 
-    mutate(pct_pat = nb_pat/nb_pat_per_grp*100,
-           pct_ei = nb_ei/nb_ei_denom*100) |> 
-    select(pt, soc, grp, nb_ei, pct_ei, nb_pat, pct_pat)
-  
-  ##### Build dataframe for visualization
-  
-  list_all <- bind_rows(df_ei_total,
-                        df_ei_pt,
-                        df_ei_soc) |> 
-    mutate(across(c("pt", "soc", "grp"), as.factor),
-           across(c("pt", "soc", "grp"), function(x) forcats::fct_relevel(x, "Total"))) |> 
-    arrange(pt, soc, grp) |> 
-    group_by(grp) |> 
-    group_split()
-  
-  df_wide <- lapply(list_all,
-                    function(df_i){
-                      grp_i <- df_i |> pull(grp) |> unique()
-                      df_i |> 
-                        select(-grp) |> 
-                        rename_with(.cols = c("nb_ei", "pct_ei", "nb_pat", "pct_pat"),
-                                    .fn = function(x) paste0(grp_i, "_", x))
-                    }) |> 
-    purrr::reduce(full_join, by = c("pt", "soc"))
+  df_wide <- desc_ei_per_soc_prepare_df(augmented_df_pat_grp = augmented_df_pat_grp,
+                                        augmented_df_pat_soc_grp = augmented_df_pat_soc_grp)
   
   ##### gt part
   
+  res <- desc_ei_per_soc_df_to_gt(df_wide = df_wide,
+                                  vec_grp = vec_grp)
+  
+  return(res)
+}
+
+
+# desc_ei_per_soc_prepare_df ----------------------------------------------
+
+#' Prepare Data for AE Description by SOC and PT
+#'
+#' This function processes patient and adverse event data to generate a wide-format summary dataframe,
+#' including total counts and percentages of events and patients per SOC (System Organ Class) and PT (Preferred Term).
+#'
+#' @param augmented_df_pat_grp A dataframe containing patient IDs and group assignments, including a "Total" group.
+#' @param augmented_df_pat_soc_grp A dataframe linking patient IDs to SOC and PT, with group assignments.
+#'
+#' @return A wide-format dataframe summarizing adverse event occurrences and patient counts across groups.
+#' @export
+#' @importFrom purrr reduce
+#'
+#' @examples
+#' \dontrun{
+#' df_wide <- desc_ei_per_soc_prepare_df(augmented_df_pat_grp, augmented_df_pat_soc_grp)
+#' }
+desc_ei_per_soc_prepare_df <- function(augmented_df_pat_grp,
+                                       augmented_df_pat_soc_grp){
+  
+  ##### compute summary statistics per group
+  
+  df_nb_pat_per_grp <- augmented_df_pat_grp |> 
+    dplyr::group_by(grp) |> 
+    dplyr::summarise(nb_pat_per_grp = n())
+  
+  df_ei_total <- augmented_df_pat_soc_grp |> 
+    dplyr::group_by(grp) |> 
+    dplyr::summarise(nb_ei = n(),
+                     pct_ei = 100,
+                     nb_pat = length(unique(id_pat)),
+                     .groups = "drop") |> 
+    dplyr::left_join(df_nb_pat_per_grp, by = "grp") |> 
+    dplyr::mutate(pct_pat = nb_pat/nb_pat_per_grp*100,
+                  pt = "Total",
+                  soc = "Total") |> 
+    dplyr::select(pt, soc, grp, nb_ei, pct_ei, nb_pat, pct_pat)
+  
+  ##### compute summary statisticsby SOC and PT
+  df_wide <- list(soc = c("grp", "soc", "pt"),
+                  pt = c("grp", "pt")) |> 
+    lapply(function(vec_grp_by){
+      temp <- augmented_df_pat_soc_grp |> 
+        dplyr::group_by(across(all_of(vec_grp_by))) |> 
+        dplyr::summarise(nb_ei = n(),
+                         nb_pat = length(unique(id_pat)),
+                         .groups = "drop") |> 
+        dplyr::left_join(df_ei_total |> 
+                           dplyr::select(grp, nb_ei_denom = nb_ei),
+                         by = "grp") |> 
+        dplyr::left_join(df_nb_pat_per_grp,
+                         by = "grp") |> 
+        dplyr::mutate(pct_pat = nb_pat/nb_pat_per_grp*100,
+                      pct_ei = nb_ei/nb_ei_denom*100)
+      
+      if(!"soc" %in% colnames(temp)){
+        temp$soc <- "Total"
+      }
+      
+      res <- temp |> 
+        dplyr::select(pt, soc, grp, nb_ei, pct_ei, nb_pat, pct_pat)
+      
+      return(res)
+    }) |> 
+    bind_rows() |> 
+    ### add total ei dataset
+    bind_rows(df_ei_total) |> 
+    ### Arrange dataframe for visualization
+    mutate(across(c("pt", "soc", "grp"), as.factor),
+           across(c("pt", "soc", "grp"), function(x) forcats::fct_relevel(x, "Total"))) |> 
+    dplyr::arrange(pt, soc, grp) |> 
+    ### Go to wide format with correct names
+    dplyr::group_by(grp) |> 
+    dplyr::group_split() |> 
+    lapply(function(df_i){
+      grp_i <- df_i |> dplyr::pull(grp) |> unique()
+      df_i |> 
+        dplyr::select(-grp) |> 
+        dplyr::rename_with(.cols = c("nb_ei", "pct_ei", "nb_pat", "pct_pat"),
+                           .fn = function(x) paste0(grp_i, "_", x))
+    }) |> 
+    purrr::reduce(full_join, by = c("pt", "soc"))
+  
+  return(df_wide)
+  
+}
+
+
+# desc_ei_per_soc_df_to_gt ------------------------------------------------
+
+#' Convert AE Summary Data to GT Table
+#'
+#' This function takes a prepared wide-format dataframe summarizing adverse events and patients
+#' and converts it into a formatted `gt` table for easy visualization.
+#'
+#' @param df_wide A wide-format dataframe containing summarized AE data.
+#' @param vec_grp A character vector of group names for which AE data is presented.
+#'
+#' @return A `gt` table formatted with appropriate labels, spans, and styling.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' gt_table <- desc_ei_per_soc_df_to_gt(df_wide, vec_grp)
+#' }
+desc_ei_per_soc_df_to_gt <- function(df_wide,
+                                     vec_grp){
   ### Create the gt table and labels
   
   gt_temp <- df_wide |> 
-    group_by(pt) |> 
+    dplyr::group_by(pt) |> 
     gt::gt() |> 
     gt::cols_label(
       pt = "PT",
-      soc = gt::md("**Evénements indésirables**"),
+      soc = gt::md("**Ev\u00e9nements ind\u00e9sirables**"),
       dplyr::ends_with("nb_ei") ~ gt::md("**N**"),
       dplyr::ends_with("nb_pat") ~ gt::md("**N**"),
       dplyr::ends_with("pct_ei") ~ gt::md("**%**"),
@@ -140,7 +194,7 @@ desc_ei_per_soc <- function(df_soc_pt,
     )
   
   gt_temp2 <- gt_temp
-  for (grp in unique(augmented_df_pat_grp$grp)) {
+  for (grp in vec_grp) {
     gt_temp2 <- gt_temp2 |> 
       gt::tab_spanner(label = gt::md("**EI**"),
                       id = glue::glue("EI_{grp}"),
@@ -182,3 +236,4 @@ desc_ei_per_soc <- function(df_soc_pt,
   
   return(res)
 }
+
