082e62e23f73fce2752ba4945419f5a7bfa1d912
FERTE Thomas <fertet@chu-bordeaux.fr>
2025-05-20 16:15:16 +0200

    fix bug

--- R/desc_ei_per_grade.R
+++ R/desc_ei_per_grade.R
@@ -65,7 +65,8 @@ desc_ei_per_grade <- function(df_pat_grp,
   if(anyNA(df_pat_grade)){
     warning("Missing data removed from df_pat_grade please be careful ! If grade is unknown, replace missing data by 'Unknown'.")
     df_pat_grade <- df_pat_grade |> 
-      mutate(grade = if_else(is.na(grade), "Unknown", grade)) |> 
+      dplyr::mutate_at(.vars = c("grade"),
+                       .funs = function(x) if_else(is.na(x), " Unknown", x)) |> 
       na.omit()
   }
   
