c92c5f410abf476c21440868b069dcd08ac300de
FERTE Thomas <fertet@chu-bordeaux.fr>
2025-07-01 17:48:13 +0200

    merge percent and N for adverse events

--- R/desc_ei_per_grade.R
+++ R/desc_ei_per_grade.R
@@ -190,7 +190,11 @@ desc_ei_per_grade_prepare_df <- function(augmented_df_pat_grp,
     mutate(grade = as.factor(grade),
            grade = forcats::fct_relevel(grade, "Any grade"),
            grp = as.factor(grp),
-           grp = forcats::fct_relevel(grp, "Total")) |> 
+           grp = forcats::fct_relevel(grp, "Total")) |>
+    ### merge n and pct
+    mutate(EI = paste0(nb_ei, " (", round(pct_ei), "%)"),
+           PAT = paste0(nb_pat, " (", round(pct_pat), "%)"),
+           .keep = "unused") |> 
     dplyr::arrange(grade, grp) |> 
     ### Go to wide format with correct names
     dplyr::group_by(grp) |> 
@@ -199,7 +203,7 @@ desc_ei_per_grade_prepare_df <- function(augmented_df_pat_grp,
       grp_i <- df_i |> dplyr::pull(grp) |> unique()
       df_i |> 
         dplyr::select(-grp) |> 
-        dplyr::rename_with(.cols = c("nb_ei", "pct_ei", "nb_pat", "pct_pat"),
+        dplyr::rename_with(.cols = c("EI", "PAT"),
                            .fn = function(x) paste0(grp_i, "_", x))
     }) |> 
     purrr::reduce(full_join, by = c("grade"))
@@ -228,35 +232,22 @@ desc_ei_per_grade_df_to_gt <- function(df_wide,
     gt::gt() |> 
     gt::cols_label(
       grade = gt::md("**Grade**"),
-      dplyr::ends_with("nb_ei") ~ gt::md("**N**"),
-      dplyr::ends_with("nb_pat") ~ gt::md("**N**"),
-      dplyr::ends_with("pct_ei") ~ gt::md("**%**"),
-      dplyr::ends_with("pct_pat") ~ gt::md("**%**")
+      dplyr::ends_with("EI") ~ gt::md("**AE <br> N (%)**"),
+      dplyr::ends_with("PAT") ~ gt::md("**Patient <br> N (%)**")
     )
   
   gt_temp2 <- gt_temp
   for (grp in vec_grp) {
     gt_temp2 <- gt_temp2 |> 
-      gt::tab_spanner(label = gt::md("**EI**"),
-                      id = glue::glue("EI_{grp}"),
-                      columns = c(glue::glue("{grp}_nb_ei"),
-                                  glue::glue("{grp}_pct_ei"))) |> 
-      gt::tab_spanner(label = gt::md("**Patients**"),
-                      id = glue::glue("Patients_{grp}"),
-                      columns = c(glue::glue("{grp}_nb_pat"),
-                                  glue::glue("{grp}_pct_pat"))) |> 
       gt::tab_spanner(label = gt::md(glue::glue("**{grp}**")),
                       id = glue::glue("over_{grp}"),
-                      columns = c(glue::glue("{grp}_nb_ei"),
-                                  glue::glue("{grp}_pct_ei"),
-                                  glue::glue("{grp}_nb_pat"),
-                                  glue::glue("{grp}_pct_pat")))
+                      columns = c(glue::glue("{grp}_EI"),
+                                  glue::glue("{grp}_PAT")))
   }
   
   ### color table
   
   res <- gt_temp2 |> 
-    gt::fmt_number(decimals = 0, columns = dplyr::matches("pct_")) |> 
     gt::sub_missing(missing_text = "") |> 
     gt::cols_align(align = "left",
                    columns = dplyr::everything()) |> 
--- R/desc_ei_per_pt.R
+++ R/desc_ei_per_pt.R
@@ -218,7 +218,10 @@ desc_ei_per_pt_prepare_df <- function(augmented_df_pat_grp,
              across(c("pt", "soc", "grp"), function(x) forcats::fct_relevel(x, "Total")))
   }
   
-  df_wide <- df_wide_temp_ordered |> 
+  df_wide <- df_wide_temp_ordered |>
+    mutate(EI = paste0(nb_ei, " (", round(pct_ei),"%)"),
+           PAT = paste0(nb_pat, " (", round(pct_pat),"%)"),
+           .keep = "unused") |> 
     dplyr::arrange(soc, pt, grp) |> 
     ### Go to wide format with correct names
     dplyr::group_by(grp) |> 
@@ -227,7 +230,7 @@ desc_ei_per_pt_prepare_df <- function(augmented_df_pat_grp,
       grp_i <- df_i |> dplyr::pull(grp) |> unique()
       df_i |> 
         dplyr::select(-grp) |> 
-        dplyr::rename_with(.cols = c("nb_ei", "pct_ei", "nb_pat", "pct_pat"),
+        dplyr::rename_with(.cols = c("EI", "PAT"),
                            .fn = function(x) paste0(grp_i, "_", x))
     }) |> 
     purrr::reduce(full_join, by = c("pt", "soc")) |> 
@@ -277,35 +280,22 @@ desc_ei_per_pt_df_to_gt <- function(df_wide,
     gt::cols_label(
       pt = gt::md(label_pt),
       soc = "soc",
-      dplyr::ends_with("nb_ei") ~ gt::md("**N**"),
-      dplyr::ends_with("nb_pat") ~ gt::md("**N**"),
-      dplyr::ends_with("pct_ei") ~ gt::md("**%**"),
-      dplyr::ends_with("pct_pat") ~ gt::md("**%**")
+      dplyr::ends_with("EI") ~ gt::md("**AE <br> N (%)**"),
+      dplyr::ends_with("PAT") ~ gt::md("**Patient <br> N (%)**")
     )
   
   gt_temp2 <- gt_temp
   for (grp in vec_grp) {
     gt_temp2 <- gt_temp2 |> 
-      gt::tab_spanner(label = gt::md("**EI**"),
-                      id = glue::glue("EI_{grp}"),
-                      columns = c(glue::glue("{grp}_nb_ei"),
-                                  glue::glue("{grp}_pct_ei"))) |> 
-      gt::tab_spanner(label = gt::md("**Patients**"),
-                      id = glue::glue("Patients_{grp}"),
-                      columns = c(glue::glue("{grp}_nb_pat"),
-                                  glue::glue("{grp}_pct_pat"))) |> 
       gt::tab_spanner(label = gt::md(glue::glue("**{grp}**")),
                       id = glue::glue("over_{grp}"),
-                      columns = c(glue::glue("{grp}_nb_ei"),
-                                  glue::glue("{grp}_pct_ei"),
-                                  glue::glue("{grp}_nb_pat"),
-                                  glue::glue("{grp}_pct_pat")))
+                      columns = c(glue::glue("{grp}_EI"),
+                                  glue::glue("{grp}_PAT")))
   }
   
   ### color table
   
   res <- gt_temp2 |> 
-    gt::fmt_number(decimals = 0, columns = dplyr::matches("pct_")) |> 
     gt::sub_missing(missing_text = "") |> 
     gt::cols_align(align = "left",
                    columns = dplyr::everything()) |> 
