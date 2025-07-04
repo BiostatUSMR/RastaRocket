46df884f59d1e352a54a971adc730d0d12851905
KRET Marion <izottem@chu-bordeaux.fr>
2025-07-04 10:34:48 +0200

    suite modif VT et MK

--- R/desc_var_function.R
+++ R/desc_var_function.R
@@ -27,6 +27,7 @@
 #'   - `FALSE`: No missing data displayed
 #'   - `TRUE`: Missing data displayed
 #'   - `NULL` (default): will be switch to `anyNA(data1)` value.
+#' @param var_tot A character string for the title of the total column. Defaults to `"Total"`.
 #'
 #' @details
 #' The function processes the dataset according to the specified parameters and generates descriptive tables.
@@ -77,8 +78,9 @@
 # show_missing_data = FALSE
 # show_n_per_group = TRUE
 # add_total = TRUE
+# var_tot = "Total"
 
-desc_var <- ## Les arugments de la fonction
+desc_var <- ## Les arguments de la fonction
   function(data1,
            ##  Jeux de données numéro 1.
            table_title = "",
@@ -99,7 +101,8 @@ desc_var <- ## Les arugments de la fonction
            freq_relevel = FALSE,
            tests = FALSE,
            show_n_per_group = FALSE,
-           show_missing_data = NULL) {
+           show_missing_data = NULL,
+           var_tot = "Total") {
     
     ### Add labels
     if(is.null(show_missing_data)){
@@ -125,7 +128,8 @@ desc_var <- ## Les arugments de la fonction
                            show_n_per_group = show_n_per_group,
                            group_title = group_title,
                            table_title = table_title,
-                           var_title = var_title)
+                           var_title = var_title,
+                           var_tot = var_tot)
     
     # Add tests tests 
     if (is.list(tests)) {
