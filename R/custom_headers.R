f458663c18b8a023c0e4aafdf89842fb724c6e37
FERTE Thomas <fertet@chu-bordeaux.fr>
2025-07-24 15:43:44 +0200

    update with show_n possible

--- R/custom_headers.R
+++ R/custom_headers.R
@@ -7,6 +7,8 @@
 #' @param base_table_missing A `gtsummary` table object (typically output from previous steps).
 #' @param var_characteristic Optional. A string to label the features column.
 #' @param show_missing_data Logical. If `TRUE`, adds missing data info to column headers.
+#' @param show_n_per_group A boolean indicating whether to display group sizes (n) for 
+#'        each level of the grouping variable.
 #' @param var_tot Optional. A string to label the total column.
 #' @param var_group Optional. Name of a grouping variable for adding a spanning header.
 #' @param group_title Optional. Title for the spanning header. If `NULL`, uses the variable label or name.
@@ -17,6 +19,7 @@
 custom_headers <- function(base_table_missing,
                            var_characteristic = NULL,
                            show_missing_data = TRUE,
+                           show_n_per_group = TRUE,
                            var_tot = NULL,
                            var_group = NULL,
                            group_title = NULL,
@@ -28,8 +31,12 @@ custom_headers <- function(base_table_missing,
   }
   
   # Set stats columns header
+  char_header <- if_else(condition = show_n_per_group,
+          true = "**{level}**<br>N = {n}",
+          false = "**{level}**")
+  
   base_table_missing <- base_table_missing |>
-    gtsummary::modify_header(gtsummary::all_stat_cols() ~ paste0("**{level}**"))
+    gtsummary::modify_header(gtsummary::all_stat_cols() ~ char_header)
   
   # Set total column header
   if (!is.null(var_tot)) {
